29e63c89eacddc1f1a4e1dd7acbc3fb7b4fee6b5
noteroad <108969311+noteroad@users.noreply.github.com>
2022-08-19 22:13:56 +0900

    first commit
    처음 올리는 동물 소리 api

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+
+app.use(cors()) //비워두면 모든 요청에 대해 허용
+
+app.get('/', function (req, res) {
+  res.send('Hello World 한글')
+})
+
+//params은 콜론 뒤에 문자로 받는다.
+app.get('/user/:id', function (req, res) {
+    const q = req.params;
+    console.log(q);
+    res.json({'userid' : q.id});
+  })
+//query는 
+  app.get('/name/:id', function (req, res) {
+    const q = req.query;
+    console.log(q.q);
+    console.log(q.name);
+    res.json({'userid' : q.name});
+
+  })
+  app.get('/sound/:name', (req, res) => {
+    const { name } = req.params;
+    if(name == "dog"){
+        res.json({'sound':'왈왈'})
+    } else if(name=="cat"){
+        res.json({'sound':'야옹'})
+    } else if(name=="pig"){
+        res.json({'sound':'오잉크오잉크'})
+    } else {
+        res.json({'sound':'알 수 없음'})
+    }
+  })
+
+app.get('/dog', function (req, res) {
+    res.json({'sound':'멍멍'})
+  })
+
+  app.get('/cat', function (req, res) {
+    res.send({'sound':'야옹'})
+  })
+
+app.listen(3000)
